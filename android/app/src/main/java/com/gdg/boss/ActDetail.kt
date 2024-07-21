96453bdf2eb382137ef848a8193fa58136f58d7e
Woojae Heo <h69088@gmail.com>
2024-07-21 15:23:34 +0900

    APP - Detail Activity 추가, viewBinding 추가

---
+++ android/app/src/main/java/com/gdg/boss/ActDetail.kt
@@ -0,0 +1,18 @@
+package com.gdg.boss
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gdg.boss.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ActDetail : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
