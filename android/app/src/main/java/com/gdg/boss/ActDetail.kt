68c9768a5ba834ecc81f9feff448e4b8c7a4a5f7
Woojae Heo <h69088@gmail.com>
2024-07-21 16:56:40 +0900

    UI - Detail page 완료

--- android/app/src/main/java/com/gdg/boss/ActDetail.kt
+++ android/app/src/main/java/com/gdg/boss/ActDetail.kt
@@ -1,18 +1,67 @@
 package com.gdg.boss
 
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
-import com.gdg.boss.databinding.ActivityMainBinding
+import com.gdg.boss.databinding.ActDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ActDetail : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    /** onBackPressed Callback */
+    private var backPressedCallback: OnBackPressedCallback? = null
+    private lateinit var binding: ActDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initData()
+        initView()
+        setOnClickListener()
+    }
+
+    /**
+     * Init Data
+     */
+    private fun initData() {
+
+        with(binding) {
+
+        }
+    }
+
+    /**
+     * Init View
+     */
+    private fun initView() {
+
+        with(binding) {
+
+            /** onBackPressed Callback */
+            backPressedCallback = object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+
+                    finish()
+                }
+            }
+        }
+    }
+
+    /**
+     * Init SetOnClickListener
+     */
+    private fun setOnClickListener() {
+
+        with(binding) {
+
+            // 뒤로가기
+            ivBack.setOnClickListener {
+
+                onBackPressedDispatcher.onBackPressed()
+            }
+        }
     }
 }
